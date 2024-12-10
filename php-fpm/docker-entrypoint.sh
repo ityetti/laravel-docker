441b26a467789e56a184981b32c2c42b695f4b58
Borovyk Oleksii <Borovyk@MacBookPro.local>
2024-12-10 21:53:50 +0100

    Fixes for php containers

--- php-fpm/docker-entrypoint.sh
+++ php-fpm/docker-entrypoint.sh
@@ -3,14 +3,20 @@
 [ "$DEBUG" = "false" ] && set -x
 
 PHP_EXT_DIR=/usr/local/etc/php/conf.d
-
-# Enable PHP extensions
 PHP_EXT_COM_ON=docker-php-ext-enable
 
+# Clean up old configurations
 [ -d ${PHP_EXT_DIR} ] && rm -f ${PHP_EXT_DIR}/docker-php-ext-*.ini
 
+# Enable extensions
 if [ -x "$(command -v ${PHP_EXT_COM_ON})" ] && [ ! -z "${PHP_EXTENSIONS}" ]; then
-  ${PHP_EXT_COM_ON} ${PHP_EXTENSIONS}
+  for ext in ${PHP_EXTENSIONS}; do
+    if [ -f "$(php-config --extension-dir)/${ext}.so" ]; then
+      ${PHP_EXT_COM_ON} ${ext}
+    else
+      echo "Extensions ${ext} not found, skip."
+    fi
+  done
 fi
 
 exec "$@"
